7b73c821c65bd4063251547c483c6be61e41bdae
SS <srodriguez841@hotmail.com>
2024-11-06 22:23:01 -0500

    Updated AdvancedSupportHandler

--- ChainOfResponsibilityApp/Handlers/AdvancedSupportHandler.cs
+++ ChainOfResponsibilityApp/Handlers/AdvancedSupportHandler.cs
@@ -4,6 +4,8 @@ namespace ChainOfResponsibilityApp.Handlers
 {
     public class AdvancedSupportHandler : ISupportHandler
     {
+        private ISupportHandler? _nextHandler;
+
         public void HandleRequest(string request)
         {
             Console.WriteLine("AdvancedSupportHandler: Checking request...");
@@ -12,16 +14,21 @@ namespace ChainOfResponsibilityApp.Handlers
             {
                 Console.WriteLine("AdvancedSupportHandler: Request handled at the advanced level.");
             }
+            else if (_nextHandler != null)
+            {
+                Console.WriteLine("AdvancedSupportHandler: Request cannot be handled. No further handlers available.");
+                _nextHandler.HandleRequest(request);
+            }
             else
             {
                 Console.WriteLine("AdvancedSupportHandler: Request cannot be handled. No further handlers available.");
-                Console.WriteLine("AdvancedSupportHandler: End of chain reached. No handler could process this request.");
             }
         }
 
         public void SetNext(ISupportHandler nextHandler)
         {
             // No next handler, as this is the last level in the chain
+            _nextHandler = nextHandler;
         }
     }
 }
